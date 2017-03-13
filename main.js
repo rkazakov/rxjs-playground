5941b1acc83beabe5d3989c782bf5c3eb2bf0278
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2017-03-13 23:53:02 +1100

    Observable operations

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var rxjs_1 = require("rxjs");
+var Observable_1 = require("rxjs/Observable");
+require("rxjs/add/operator/map");
+require("rxjs/add/operator/filter");
 var numbers = [1, 5, 10];
-var source = rxjs_1.Observable.create(function (observer) {
-    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-        var n = numbers_1[_i];
-        observer.next(n);
-    }
-    observer.complete();
-});
+var source = Observable_1.Observable.create(function (observer) {
+    var index = 0;
+    var produceValue = function () {
+        observer.next(numbers[index++]);
+        if (index < numbers.length) {
+            setTimeout(produceValue, 200);
+        }
+        else {
+            observer.complete();
+        }
+    };
+    produceValue();
+}).map(function (n) { return n * 2; })
+    .filter(function (n) { return n > 4; });
 source.subscribe(function (value) { return console.log("value: " + value); }, function (e) { return console.log("error: " + e); }, function () { return console.log('complete'); });
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
-import {Observable, Observer} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
+// Functions with side-effects
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/filter";
 
 let numbers = [1, 5, 10];
 
 // Promise | Array
 let source = Observable.create(observer => {
-  for(let n of numbers) {
-    observer.next(n);
+  let index = 0;
+  let produceValue = () => {
+    observer.next(numbers[index++]);
+
+    if (index < numbers.length) {
+      setTimeout(produceValue, 200);
+    }
+    else {
+      observer.complete();
+    }
   }
-  observer.complete();
-});
+
+  produceValue();
+}).map(n => n * 2)
+.filter(n => n > 4);
 
 source.subscribe(
   value => console.log(`value: ${value}`),
